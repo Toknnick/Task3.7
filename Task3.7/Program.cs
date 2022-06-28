8e7bb1a209c08029e067e426d4f132dff2a4debd
Maksim <maksimus.tumannick@yandex.ru>
2022-06-28 11:46:47 +0300

    Task3.7

---
+++ Task3.7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "I am using visual studio";
+            string[] words = phrase.Split(' ');
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
